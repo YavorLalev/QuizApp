4dd8917443b3c4f025ec7275d99375b2b98545d7
YavorLalev <yavorlalev@abv.bg>
2023-11-28 08:43:31 +0100

    add-profile.js

---
+++ profile.js
@@ -0,0 +1,7 @@
+const bodyElement = document.querySelector('[data-js="body"]');
+const buttonOne = document.querySelector('[data-js="toggle-button"]');
+
+buttonOne.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  console.log(buttonOne);
+});
