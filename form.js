5394adbe1fab8c05a396adae4132880854002f50
YavorLalev <yavorlalev@abv.bg>
2023-11-28 23:39:05 +0100

    add-javascrip -form-and-fix-css-items

---
+++ form.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+});
